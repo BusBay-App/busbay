29561a1fabe3b1381fa65fbad96f59455531044d
Atharva Bhanage <atharvabhanage1@gmail.com>
2022-07-07 14:41:16 +0530

    aa

--- app/src/main/java/com/example/busbay/SignIn.kt
+++ app/src/main/java/com/example/busbay/SignIn.kt
@@ -17,7 +17,7 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-
+//this is branch1
 class SignIn : AppCompatActivity() {
 
     private companion object{
