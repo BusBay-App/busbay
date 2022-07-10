8dd5d3369d306a06c7df674e6e915f7b8675cedb
Atharva Bhanage <atharvabhanage1@gmail.com>
2022-07-10 20:45:28 +0530

    map implemented

--- app/src/main/java/com/example/busbay/SignIn.kt
+++ app/src/main/java/com/example/busbay/SignIn.kt
@@ -17,7 +17,7 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-//this is branch4
+//this is branch6
 class SignIn : AppCompatActivity() {
 
     private companion object{
