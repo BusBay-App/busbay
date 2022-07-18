b8729be03d0b6141f3fd540caa87453e8590dd52
Atharva Bhanage <atharvabhanage1@gmail.com>
2022-07-18 09:28:12 +0530

    aa

--- app/src/main/java/com/example/busbay/PollFragment.kt
+++ app/src/main/java/com/example/busbay/PollFragment.kt
@@ -7,7 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
 import androidx.preference.PreferenceManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.busbay.databinding.FragmentMapBinding
 import com.example.busbay.databinding.FragmentPollBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -22,6 +30,15 @@ class PollFragment : Fragment() {
     lateinit var addPoll: FloatingActionButton //
 
 
+    lateinit var readProgressLayout: RelativeLayout
+    lateinit var readProgressBar : ProgressBar
+    lateinit var recyclerView: RecyclerView
+    lateinit var layoutManager: LinearLayoutManager
+    lateinit var recyclerAdapter:PollRVAdapter
+
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,11 +47,80 @@ class PollFragment : Fragment() {
         binding=FragmentPollBinding.inflate(inflater,container,false)
         //Firebase auth
         auth= Firebase.auth//different UI
-
-
         addPoll=binding.floatingActionButtonPoll
 
+
+
+        recyclerView=binding.recyclerView
+        readProgressLayout=binding.readProgressLayout
+        readProgressBar=binding.readProgressBar
+        layoutManager= LinearLayoutManager(requireActivity())
+
+
+        val bookList= arrayListOf<BookofPoll>()
+        val queue= Volley.newRequestQueue(requireActivity())
+        val url="https://script.google.com/macros/s/AKfycbyWva0kwG5EbOYbLb1OV2G92Xn7sBTEodw6oPo5bqV5Z77dYm1QDIgMN4rqI8rY34zOZA/exec"
+
+        val jsonObjectRequest=object : JsonObjectRequest(
+            Request.Method.GET,url,null,
+            Response.Listener {
+                val data=it.getJSONArray("items")
+                for(i in 0 until data.length()) {
+                    val bookJasonObject = data.getJSONObject(i)
+                    val listfAlltags =
+                        bookJasonObject.getString("itembranchyear").split(" ").toTypedArray()
+
+                    for (i in 0 until listfAlltags.size) {
+                        if(listfAlltags[i]==""){
+                            break
+                        }
+
+                    val bookObject = BookofPoll(
+                        bookJasonObject.getString("date"),
+                        bookJasonObject.getString("itememail_id"),
+//                        bookJasonObject.getString("itembranchyear"),
+                        listfAlltags[i],
+                        bookJasonObject.getString("itemquestion"),
+                        bookJasonObject.getString("itemoptn1"),
+                        bookJasonObject.getString("itemoptn2"),
+                        bookJasonObject.getString("itemoptn3"),
+                        bookJasonObject.getString("itemoptn4"),
+                        bookJasonObject.getString("itemoptn5"),
+                        bookJasonObject.getString("itemoptn6"),
+                        bookJasonObject.getString("itemoptn7"),
+                        bookJasonObject.getString("itemc1"),
+                        bookJasonObject.getString("itemc2"),
+                        bookJasonObject.getString("itemc3"),
+                        bookJasonObject.getString("itemc4"),
+                        bookJasonObject.getString("itemc5"),
+                        bookJasonObject.getString("itemc6"),
+                        bookJasonObject.getString("itemc7")
+                    )
+//                    if(bookJasonObject.getString("itemRating")=="2"){
+                    bookList.add(bookObject)
+//                    }
+                }
+                }
+
+                readProgressLayout.visibility= View.GONE
+                readProgressBar.visibility= View.GONE
+                recyclerAdapter= PollRVAdapter(requireActivity(),bookList)
+                recyclerView.adapter=recyclerAdapter
+                recyclerView.layoutManager=layoutManager
+            }, Response.ErrorListener {  }
+        ){
+            override fun getHeaders(): MutableMap<String, String> {
+                return super.getHeaders()
+            }
+        }
+        queue.add(jsonObjectRequest)
+
+
+
+
+
         addPoll.setOnClickListener { GotoAddPollactivity()}
+
 
 
 
---
+++ app/src/main/java/com/example/busbay/PollRVAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.busbay
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PollRVAdapter(val context: Context, val itemList:ArrayList<BookofPoll>)
+    : RecyclerView.Adapter<PollRVAdapter.ReadViewHolder>()  {
+
+    class ReadViewHolder(view: View):RecyclerView.ViewHolder(view){
+
+        val date :TextView=view.findViewById(R.id.txtv_Date)
+        val itememail_id :TextView=view.findViewById(R.id.txtv_uploader_emailid)
+        val itembranchyear :TextView=view.findViewById(R.id.txtv_branch)
+        val itemquestion :TextView=view.findViewById(R.id.txtv_relative_question)
+        val itemoptn1 :Button=view.findViewById(R.id.opt1)
+        val itemoptn2 :Button=view.findViewById(R.id.opt2)
+        val itemoptn3 :Button=view.findViewById(R.id.opt3)
+        val itemoptn4 :Button=view.findViewById(R.id.opt4)
+        val itemoptn5 :Button=view.findViewById(R.id.opt5)
+        val itemoptn6 :Button=view.findViewById(R.id.opt6)
+        val itemoptn7 :Button=view.findViewById(R.id.opt7)
+        val itemc1 :TextView=view.findViewById(R.id.txtv_c1)
+        val itemc2 :TextView=view.findViewById(R.id.txtv_c2)
+        val itemc3 :TextView=view.findViewById(R.id.txtv_c3)
+        val itemc4 :TextView=view.findViewById(R.id.txtv_c4)
+        val itemc5 :TextView=view.findViewById(R.id.txtv_c5)
+        val itemc6 :TextView=view.findViewById(R.id.txtv_c6)
+        val itemc7 :TextView=view.findViewById(R.id.txtv_c7)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReadViewHolder {
+        val view= LayoutInflater.from(parent.context).inflate(R.layout.single_poll,parent,false)
+        return ReadViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ReadViewHolder, position: Int) {
+        val book=itemList[position]
+//        if(book.bookRating.toString()=="2"){
+        holder.date.text=book.date
+        holder.date.text=book.date
+        holder.itememail_id.text=book.itememail_id
+        holder.itembranchyear.text=book.itembranchyear
+        holder.itemquestion.text=book.itemquestion
+        holder.itemoptn1.text=book.itemoptn1
+        holder.itemoptn2.text=book.itemoptn2
+        holder.itemoptn3.text=book.itemoptn3
+        holder.itemoptn4.text=book.itemoptn4
+        holder.itemoptn5.text=book.itemoptn5
+        holder.itemoptn6.text=book.itemoptn6
+        holder.itemoptn7.text=book.itemoptn7
+        holder.itemc1.text=book.itemc1
+        holder.itemc2.text=book.itemc2
+        holder.itemc3.text=book.itemc3
+        holder.itemc4.text=book.itemc4
+        holder.itemc5.text=book.itemc5
+        holder.itemc6.text=book.itemc6
+        holder.itemc7.text=book.itemc7
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+}
